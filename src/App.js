e79e68335d9a771a54907aa0726242282bc664b3
vshtenovych <vshtenovych@roku.com>
2024-01-29 11:13:00 +0200

    Add src

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'; // Importing the CSS file for styling
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to LiveSub</h1>
+        <p>Real-Time Speech and Gesture to Text Conversion</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
